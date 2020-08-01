7d9b2d4cd20d0f076a8f4d93e0b883fa31947620
bansaltushar014 <bansaltushar014@gmail.com>
2020-08-01 22:17:01 +0530

    Readme

--- src/helper.js
+++ src/helper.js
@@ -5,8 +5,8 @@ const web3 = {
     isDefine : function() {
         console.log("called");
 
-        // CorrectOne in .env
-        // this.gweb = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/"));
+        
+        // this.gweb = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/" + "<project id from infura.io>"));
         
         this.gweb = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
     }
